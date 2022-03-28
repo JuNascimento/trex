37507735f8683c1bcf8fe7f87c2824a34599e130
Julia de Araújo Nascimento <ju.araujo11@yahoo.com.br>
2022-03-28 13:00:31 -0300

    Adiciona comentários para ajudar o entendimento dos alunos

--- sketch.js
+++ sketch.js
@@ -1,17 +1,21 @@
+// crio as variaveis
 
 
 // serve para precarregar imagens/animacoes/sons
-function preload(){
+function preload() {
 
 }
 
 
 // serve pra fazer a configuracao inicial (só é executada 1 vez quando o jogo começar)
 function setup() {
+    createCanvas(600, 200)
 
 }
 
-// serve para fazer o jogo funcionar o tempo todo (é executada o tempo todo, infinitamente até eu parar o jogo)
+// serve para fazer o jogo funcionar o tempo todo (é executada o tempo todo, infinitamente até parar o jogo)
 function draw() {
-   
+    background('white')
+
+    drawSprites()
 }
